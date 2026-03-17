dcd1f749e69aa0fb31e661d5230f83aa09822312
rihanrosihan <56996605+rihanrosihan@users.noreply.github.com>
2026-03-17 13:55:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,3 @@ app = FastAPI()
 def root():
     return {"status": "Lumpad running 🚀"}
 
-@app.get("/webhook")
-def verify():
-    return {"status": "webhook ready"}
-
-@app.post("/webhook")
-async def webhook(req: Request):
-    data = await req.json()
-    print("EVENT:", data)
-    return {"ok": True}
