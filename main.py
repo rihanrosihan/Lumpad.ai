0b29fc9a20718c432de2e7a7ae648189df8b6793
rihanrosihan <56996605+rihanrosihan@users.noreply.github.com>
2026-03-17 13:35:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "Lumpad running 🚀"}
+
+@app.get("/webhook")
+def verify():
+    return {"status": "webhook ready"}
+
+@app.post("/webhook")
+async def webhook(req: Request):
+    data = await req.json()
+    print("EVENT:", data)
+    return {"ok": True}
